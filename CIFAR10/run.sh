a51f4b497e2d7fa2a2244b800475a67d3682a5c5
Fan Min at swpu.edu.cn <30546261+FanSmale@users.noreply.github.com>
2024-03-17 17:20:57 +0800

    Create run.sh

---
+++ CIFAR10/run.sh
@@ -0,0 +1,71 @@
+echo "Running CIFAR10"
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train.py \
+    --gpu 1 \
+    --info U \
+    --balance True \
+    > log.out 2>&1 &
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_sup.py \
+    --gpu 0 \
+    --info F \
+    --balance True \
+    > log.out 2>&1 &
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train.py \
+    --gpu 1 \
+    --info F \
+    --balance False \
+    > log.out 2>&1 &
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_sup.py \
+    --gpu 1 \
+    --info F \
+    --balance False \
+    > log.out 2>&1 &
+
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_baseline.py \
+    --gpu 0 \
+    --method Random \
+    > log.out 2>&1 &
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_baseline.py \
+    --gpu 0 \
+    --method Entropy \
+    > log.out 2>&1 &
+
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_baseline.py \
+    --gpu 1 \
+    --method Margin \
+    > log.out 2>&1 &
+
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_dynamicE2_ssal.py \
+    --gpu 0 \
+    > log.out 2>&1 &
+
+cd /home/wyx/vscode_projects/SSAL/CIFAR10
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_dynamicE2_baseline.py \
+    --gpu 0 \
+    --method Random \
+    > log.out 2>&1 &
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_dynamicE2_baseline.py \
+    --gpu 0 \
+    --method Entropy \
+    > log.out 2>&1 &
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_dynamicE2_baseline.py \
+    --gpu 1 \
+    --method LeastConfidence \
+    > log.out 2>&1 &
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR10/train_dynamicE2_baseline.py \
+    --gpu 1 \
+    --method Margin \
+    > log.out 2>&1 &
